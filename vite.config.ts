1a5589c47e37e174b337a76b2076e4245f98fbc0
frrrancoelgori-ui <frrrancoelgori@gmail.com>
2026-01-29 18:49:21 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',                      // ← agrega esto (o './' si pruebas local con subdir)
   esbuild: {
     target: 'es2022'
   },
